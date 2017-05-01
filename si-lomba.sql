80ee43ca531f0f1ec6b4fa3cd0cdb20e0f6f7bf6
AHMAD-ISMAIL\Ahmad Ismail <x5ahmadismail@gmail.com>
2017-05-01 11:52:37 +0700

    Database FIX

--- si-lomba.sql
+++ si-lomba.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2017 at 06:10 AM
+-- Generation Time: May 01, 2017 at 06:51 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -164,17 +164,17 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `anggota_tim`
   ADD PRIMARY KEY (`id_anggota_tim`),
-  ADD KEY `id_tim` (`id_tim`),
-  ADD KEY `id_job` (`id_job`),
-  ADD KEY `id_user` (`id_user`);
+  ADD KEY `anggota_tim_ibfk_1` (`id_user`),
+  ADD KEY `anggota_tim_ibfk_2` (`id_job`),
+  ADD KEY `id_tim` (`id_tim`);
 
 --
 -- Indexes for table `bimbingan`
 --
 ALTER TABLE `bimbingan`
   ADD PRIMARY KEY (`id_bimbingan`),
-  ADD KEY `id_tim` (`id_tim`),
-  ADD KEY `id_dosbing` (`id_dosbing`);
+  ADD KEY `bimbingan_ibfk_1` (`id_dosbing`),
+  ADD KEY `id_tim` (`id_tim`);
 
 --
 -- Indexes for table `dosbing`
@@ -199,30 +199,31 @@ ALTER TABLE `lomba`
 --
 ALTER TABLE `request_tim`
   ADD PRIMARY KEY (`id_request`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_tim` (`id_tim`);
+  ADD KEY `request_tim_ibfk_1` (`id_user`),
+  ADD KEY `request_tim_ibfk_2` (`id_tim`);
 
 --
 -- Indexes for table `riwayat_user`
 --
 ALTER TABLE `riwayat_user`
   ADD PRIMARY KEY (`id_riwayat_user`),
-  ADD KEY `id_user` (`id_user`),
+  ADD KEY `riwayat_user_ibfk_1` (`id_user`),
   ADD KEY `id_lomba` (`id_lomba`);
 
 --
 -- Indexes for table `tim`
 --
 ALTER TABLE `tim`
-  ADD PRIMARY KEY (`id_tim`);
+  ADD PRIMARY KEY (`id_tim`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `tim_ikut_lomba`
 --
 ALTER TABLE `tim_ikut_lomba`
   ADD PRIMARY KEY (`id_tim_ikut_lomba`),
-  ADD KEY `id_tim` (`id_tim`),
-  ADD KEY `id_lomba` (`id_lomba`);
+  ADD KEY `tim_ikut_lomba_ibfk_1` (`id_lomba`),
+  ADD KEY `id_tim` (`id_tim`);
 
 --
 -- Indexes for table `user`
@@ -231,6 +232,60 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anggota_tim`
+--
+ALTER TABLE `anggota_tim`
+  MODIFY `id_anggota_tim` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bimbingan`
+--
+ALTER TABLE `bimbingan`
+  MODIFY `id_bimbingan` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `dosbing`
+--
+ALTER TABLE `dosbing`
+  MODIFY `id_dosbing` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `job`
+--
+ALTER TABLE `job`
+  MODIFY `id_job` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lomba`
+--
+ALTER TABLE `lomba`
+  MODIFY `id_lomba` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `request_tim`
+--
+ALTER TABLE `request_tim`
+  MODIFY `id_request` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `riwayat_user`
+--
+ALTER TABLE `riwayat_user`
+  MODIFY `id_riwayat_user` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tim`
+--
+ALTER TABLE `tim`
+  MODIFY `id_tim` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tim_ikut_lomba`
+--
+ALTER TABLE `tim_ikut_lomba`
+  MODIFY `id_tim_ikut_lomba` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- Constraints for dumped tables
 --
 
@@ -238,16 +293,16 @@ ALTER TABLE `user`
 -- Constraints for table `anggota_tim`
 --
 ALTER TABLE `anggota_tim`
-  ADD CONSTRAINT `anggota_tim_ibfk_1` FOREIGN KEY (`id_tim`) REFERENCES `tim` (`id_tim`),
+  ADD CONSTRAINT `anggota_tim_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
   ADD CONSTRAINT `anggota_tim_ibfk_2` FOREIGN KEY (`id_job`) REFERENCES `job` (`id_job`),
-  ADD CONSTRAINT `anggota_tim_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `anggota_tim_ibfk_3` FOREIGN KEY (`id_tim`) REFERENCES `tim` (`id_tim`);
 
 --
 -- Constraints for table `bimbingan`
 --
 ALTER TABLE `bimbingan`
-  ADD CONSTRAINT `bimbingan_ibfk_1` FOREIGN KEY (`id_tim`) REFERENCES `tim` (`id_tim`),
-  ADD CONSTRAINT `bimbingan_ibfk_2` FOREIGN KEY (`id_dosbing`) REFERENCES `dosbing` (`id_dosbing`);
+  ADD CONSTRAINT `bimbingan_ibfk_1` FOREIGN KEY (`id_dosbing`) REFERENCES `dosbing` (`id_dosbing`),
+  ADD CONSTRAINT `bimbingan_ibfk_2` FOREIGN KEY (`id_tim`) REFERENCES `tim` (`id_tim`);
 
 --
 -- Constraints for table `request_tim`
@@ -264,11 +319,17 @@ ALTER TABLE `riwayat_user`
   ADD CONSTRAINT `riwayat_user_ibfk_2` FOREIGN KEY (`id_lomba`) REFERENCES `lomba` (`id_lomba`);
 
 --
+-- Constraints for table `tim`
+--
+ALTER TABLE `tim`
+  ADD CONSTRAINT `tim_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
 -- Constraints for table `tim_ikut_lomba`
 --
 ALTER TABLE `tim_ikut_lomba`
-  ADD CONSTRAINT `tim_ikut_lomba_ibfk_1` FOREIGN KEY (`id_tim`) REFERENCES `tim` (`id_tim`),
-  ADD CONSTRAINT `tim_ikut_lomba_ibfk_2` FOREIGN KEY (`id_lomba`) REFERENCES `lomba` (`id_lomba`);
+  ADD CONSTRAINT `tim_ikut_lomba_ibfk_1` FOREIGN KEY (`id_lomba`) REFERENCES `lomba` (`id_lomba`),
+  ADD CONSTRAINT `tim_ikut_lomba_ibfk_2` FOREIGN KEY (`id_tim`) REFERENCES `tim` (`id_tim`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
