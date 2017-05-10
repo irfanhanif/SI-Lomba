d112b12f50c4533cb9b4f7f850301dc6d85fb12b
irfanhanif <if.irfanhanif@gmail.com>
2017-05-10 17:50:29 +0700

    Mengambil data user: mengubah menjadi POST

--- Juara/juara/routes/index.js
+++ Juara/juara/routes/index.js
@@ -17,10 +17,10 @@ router.post('/register', function(req, res, next){
   var register = new UserController();
   result = register.registerUser(res, req.body);
 });
-router.get('/editprofile', function(req, res, next){
+router.post('/editprofile', function(req, res, next){
   console.log(req);
   var get_profile = new UserController();
-  get_profile.getUserProfile(res, req.headers);
+  get_profile.getUserProfile(res, req.body);
 });
 
 module.exports = router;
