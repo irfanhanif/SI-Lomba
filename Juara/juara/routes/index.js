c7c710074ca773357325c4dc440c5103b91bbb95
irfanhanif <if.irfanhanif@gmail.com>
2017-05-25 16:52:24 +0700

    revisi syntax

--- Juara/juara/routes/index.js
+++ Juara/juara/routes/index.js
@@ -102,7 +102,7 @@ router.post('/upload', function(req, res, next){
   upload(req, res, function(err){
     if(err){
       var ret = {"status": "failed"};
-      res.end(ret));
+      res.end(ret);
     } else {
       var ret = {"status": "uploaded"};
       res.end(ret);
